f28ccadf995a118b81b32cb62aefedd661a866ec
codeworkx <codeworkx@cyanogenmod.org>
2015-12-22 19:12:46 +0100

    h811: move device overlay after vendor/cm
    Change-Id: I02b779a1b52e618f9ab8810c5c0b4e54c39ae830

--- cm.mk
+++ cm.mk
@@ -6,6 +6,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_NAME := cm_h811
 
--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/h811/h811-vendor.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
