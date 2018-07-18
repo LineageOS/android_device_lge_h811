41f06f94b807e814a3fd71cbb59a68fce44264c9
ThePiGuy0 <thepiguy0@gmail.com>
2018-07-18 16:28:19 +0100

    h811: Complete rebrand to Lineage for 15.1
    Change-Id: I4886de63e6c1b6a32c98e269064fcfed6e907d06

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/h811/full_h811.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
